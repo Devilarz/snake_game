9b36d32013a8ba933a4c19e46f2de2a0b74a8b6b
Devilarz <ies.nalindapubudu@gmail.com>
2022-10-05 10:57:03 +0530

    initial files added

---
+++ main.py
@@ -0,0 +1,35 @@
+from turtle import Screen
+from food import Food
+import time
+from snake import Snake
+import random
+
+screen = Screen()
+
+screen.setup(width=600, height=600)
+screen.title("My Snake Game")
+screen.bgcolor("black")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+    # Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+
+
+screen.exitonclick()
